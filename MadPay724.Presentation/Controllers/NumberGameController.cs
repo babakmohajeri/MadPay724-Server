901c61ecd99f5bd32a1d579a61c6c70b12573a7b
babakmohajeri <babak.mohajeri@gmail.com>
2019-12-03 00:36:00 +0330

    First Commit

---
+++ MadPay724.Presentation/Controllers/NumberGameController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MadPay724.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NumberGameController : ControllerBase
+    {
+        private static readonly string[] day = new string[] { "Monday", "Thuesday", "Wendsday", "Thersdauy", "Friday", "saturday", "Sunday" };
+        List<Days> myday = new List<Days>();
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> Get()
+        {
+            for (int i = 0; i < day.Length; i++)
+            {
+                Days md = new Days()
+                {
+                    DayName = day[i].ToString(),
+                    DayNumber = i + 1
+                };
+                myday.Add(md);
+            }
+            int c = myday.Count();
+            string[] myText = new string[c];
+
+            foreach (var item in myday)
+            {
+                myText[item.DayNumber - 1] = "My day is: " + item.DayName;
+            }
+            return myText;
+        }
+
+        [HttpGet("{num}")]
+        public async Task<ActionResult<IEnumerable<Days>>> Get(int num)
+        {
+            if (num > 0 && num <= 7)
+            {
+                Days md = new Days()
+                {
+                    DayName = day[num - 1].ToString(),
+                    DayNumber = num
+                };
+                myday.Add(md);
+                return myday.ToArray();
+            }
+            else
+            {
+                List<Days> mdn = null;
+                await Response.WriteAsync("Error. Please enter a number between 1-7");
+                return mdn;
+            }
+        }
+    }
+}
---
+++ MadPay724.Presentation/Days.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MadPay724.Presentation
+{
+    public class Days
+    {
+        public string DayName { get; set; }
+        public int DayNumber { get; set; }
+    }
+}
